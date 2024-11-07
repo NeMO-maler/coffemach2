e33581d8e9baa181cfc13f48179dc047ad4c9c25
Egor <kingsouplet44@gmail.com>
2024-11-07 14:15:31 +0300

    FINAL2

--- src/main/java/org/example/REFRESH.java
+++ src/main/java/org/example/REFRESH.java
@@ -4,17 +4,29 @@ import java.util.Scanner;
 
 public class REFRESH {
     public static void Refresh() {
-        System.out.println("Выберите действие: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println(".Выберите действие:");
-        System.out.println("0.Восполнить запас воды");
-        System.out.println("1.Восполнить запас воды");
-        System.out.println("2.Восполнить запас молока");
-        System.out.println("3.Восполнить запас кофе");
-        System.out.println("4.Воспроизвести очистку");
-        System.out.println("5.Воспроизвести очистку и пополнение всех ингридиентов");
-        System.out.println("6.Вернуться назад");
-        int rf = scanner.nextInt();
+        byte rf = - 1;
+        while (rf < 0 || rf > 6) {
+            System.out.println("_________________________________________________________");
+            System.out.println("Выберите действие: ");
+            System.out.println(".Выберите действие:");
+            System.out.println("0.Наличие ингридиентов");
+            System.out.println("1.Восполнить запас воды");
+            System.out.println("2.Восполнить запас молока");
+            System.out.println("3.Восполнить запас кофе");
+            System.out.println("4.Воспроизвести очистку");
+            System.out.println("5.Воспроизвести очистку и пополнение всех ингридиентов");
+            System.out.println("6.Вернуться назад");
+            System.out.println("_________________________________________________________");
+
+                try {
+                    String input = scanner.nextLine();
+                    rf =Byte.parseByte(input);
+
+                } catch (NumberFormatException e) {
+                    System.out.println( "Введите корректное значение указаное выше ");
+                }
+        }
         switch (rf) {
             case 1:
                 MainMenu.water = MainMenu.maxWater;
@@ -35,10 +47,12 @@ public class REFRESH {
                 MainMenu.polution = 0;
                 Refresh();
             case 0:
+                System.out.println("_________________________________________________________");
                 System.out.println("Текущее кол-во воды:" + MainMenu.water);
                 System.out.println("Текущее кол-во кофе:" + MainMenu.Coffe);
                 System.out.println("Текущее кол-во молока:" + MainMenu.milk);
                 System.out.println("Состояние загрязнение:" + MainMenu.polution);
+                System.out.println("_________________________________________________________");
         }
 
 
